5cac51c4192887a3107eb73a0e9888b2b34e1da0
keerthiv1119 <44571328+keerthiv1119@users.noreply.github.com>
2020-04-04 23:53:40 +0530

    Add files via upload

---
+++ Day4_MoveZeroes.py
@@ -0,0 +1,10 @@
+nums = [0,1,0,3,12]
+count = 0
+for i in range(len(nums)):
+    if nums[i] != 0:
+        nums[count] = nums[i]
+        count += 1
+while(count < len(nums)):
+    nums[count] = 0
+    count += 1
+print(nums)
